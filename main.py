29b3a759b3ceb9d99487b0cb745874fd2ade0aba
Emiliano Muratore <120816866+EmeehM@users.noreply.github.com>
2026-01-14 10:13:44 -0300

    hotfix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ logger = get_logger(__name__)
 def main():
     logger.info('Start crawling...')
     crawler.Crawler().crawl()
-    logger.info('End crawling...')
+    logger.info('End crawling..')
 
 if __name__ == '__main__':
     main()
